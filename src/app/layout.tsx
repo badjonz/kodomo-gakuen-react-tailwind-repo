e69f793e2e6b974526a3ba1b4ab876f6b78ac808
Jon Kumar <badjonz@gmail.com>
2025-07-15 17:59:37 +0900

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
 import type { Metadata } from "next";
-import { DM_Sans } from "next/font/google";
+import { Noto_Sans_JP } from 'next/font/google';
+// import { Kosugi } from "next/font/google";
 import "./globals.css";
-import clsx from "clsx";
+// import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
 
-const dmSans = DM_Sans({ subsets: ["latin"] });
+const noto = Noto_Sans_JP({
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
-  title: "Light Saas Landing Page",
-  description: "Template created by Frontend Tribe",
+  title: "Kodomo Gakuen",
+  description: "Template created by Jon Kumar",
 };
 
 export default function RootLayout({
@@ -17,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="relative">
-      <body className={clsx(dmSans.className, "antialiased bg-[#EAEEFE]")}>
+      <body className={twMerge(noto.className, "antialiased bg-[#FAF9F9]")}>
         {children}
       </body>
     </html>
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,24 @@
-export const Navbar = () => {
-  return null;
+export const Header = () => {
+  return <div className="flex flex-col">
+    <div className="flex justify-between h-[30px] bg-[rgba(0,174,255,0.85)] px-[60px] text-white text-[11px] items-center fixed w-full z-50">
+      <a href=""> info@kodomogakuen.com</a>
+      <a href="">English</a>
+    </div>
+    <div className="fixed top-[30px] w-full z-50 bg-[rgba(0,0,0,0.1)] h-[75px]">
+      <nav className="flex justify-between items-center h-full px-[60px]">
+        <div>
+          <a href=""><span className="text-[#32CD32]">test</span></a>
+        </div>
+        <ul className="flex gap-[30px] text-[14px] font-bold text-white font-medium">
+          <li><a href="">ホーム</a></li>
+          <li><a href="">インフォメーション</a></li>
+          <li><a href="">書類</a></li>
+          <li><a href="">クラス</a></li>
+          <li><a href="">お知らせ・ニューズ</a></li>
+          <li><a href="">課外教室</a></li>
+        
+        </ul>
+      </nav>
+    </div>
+  </div>;
 };
