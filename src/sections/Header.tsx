1b54df855cca34d8092818cc93a08549066c9637
Jon Kumar <badjonz@gmail.com>
2025-07-23 15:45:48 +0900

    I started working on converting the menu with tailwind classes

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -10,14 +10,96 @@ export const Header = () => {
         <div>
           <a href=""><span className="text-[#32CD32]">test</span></a>
         </div>
-        <ul className="flex gap-[35px] text-[14px] font-bold text-white font-medium">
+        {/* <ul className="flex gap-[35px] text-[14px] font-bold text-white font-medium">
           <li><a href="">ホーム</a></li>
           <li><a href="">インフォメーション</a></li>
           <li><a href="">書類</a></li>
           <li><a href="">クラス</a></li>
           <li><a href="">課外教室</a></li>
         
-        </ul>
+        </ul> */}
+        <ul className="flex w-full items-center justify-center text-white h-full">
+            <li className="relative mx-[3px] flex -z-10"><a className="text-[1.4rem] p-[1.3rem_2rem] rounded-[3px] transition-all duration-200 w-full" href="./index.html"><span>ホーム</span> </a></li>
+            
+            <li className="nav-menu__item nav-menu__item--2">
+              <a className="nav-menu__link nav-menu__link--2" href="#" data-nav="1">インフォメーション</a>
+              <ul className="submenu-list">
+                <li className="submenu__item">
+                  <a href="./about.html" className="submenu__link">
+                    <svg className="navbar__icon">
+                      
+                    </svg>
+                     <span>保育方針</span> 
+                  </a>
+                </li>
+                <li className="submenu__item">
+                  <a href="./fees.html" className="submenu__link">
+                    <svg className="navbar__icon">
+                      
+                    </svg>
+
+
+                    <span>保育料</span>
+                  </a>
+                </li>
+                <li className="submenu__item">
+                  <a href="./privacy.html" className="submenu__link">
+                    <svg className="navbar__icon">
+                      
+                    </svg>
+
+
+                    <span>プライバシーポリシー</span>
+                  </a>
+                </li>
+                <li className="submenu__item">
+                  <a href="./menu.html" className="submenu__link">
+                    <svg className="navbar__icon">
+                      
+                    </svg>
+
+
+                    <span>給食</span>
+                  </a>
+                </li>
+              </ul>
+            </li>
+       
+            <li className="nav-menu__item nav-menu__item--3">
+              <a className="nav-menu__link nav-menu__link--3" href="./forms.html" data-nav="2">書類</a>
+              
+            </li>
+           
+            <li className="nav-menu__item nav-menu__item--4">
+              <a className="nav-menu__link nav-menu__link--4" href="#" data-nav="3">クラス</a>
+              <ul className="submenu-list">
+                <li className="submenu__item">
+                  <a href="./nyuuji.html" className="submenu__link"><svg className="navbar__icon">
+                    
+                  </svg><span>乳児</span> </a>
+                </li>
+                <li className="submenu__item">
+                  <a href="./youji.html" className="submenu__link"><svg className="navbar__icon">
+                    
+                  </svg><span>幼児</span> </a>
+                </li>
+                <li className="submenu__item">
+                  <a href="./star.html" className="submenu__link"><svg className="navbar__icon">
+                    
+                  </svg><span>国際クラス</span> </a>
+                </li>
+              </ul>
+            </li>
+            
+            <li className="nav-menu__item nav-menu__item--5"><a className="nav-menu__link" href="./blog.html">お知らせ・ニューズ</a></li>
+            
+            <li className="nav-menu__item nav-menu__item--6">
+              <a className="nav-menu__link" href="./activities.html">課外教室</a>
+            </li>
+            
+          </ul>
+          
+
       </nav>
     </div>
   </div>;
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,16 +1,47 @@
 "use client";
 import Image from 'next/image';
 import heroImage from '@assets/hero-image.jpg';
-export const Hero = () => {
-  return <div className="h-screen text-white relative overflow-auto before:content-[''] before:absolute before:inset-0 before:bg-black/45 before:z-10">
-  {/* Background Image */}
-  <Image 
-    src={heroImage} 
-    alt="Hero Image" 
-    layout="fill" 
-    objectFit="cover" 
-    className="z-0" 
-  />
 
-</div>;
+export const Hero = () => {
+  return (
+    <div className="h-screen text-white relative overflow-auto before:content-[''] before:absolute before:inset-0 before:bg-black/45 before:z-10">
+      <Image 
+        src={heroImage} 
+        alt="Hero Image" 
+        fill
+        className="z-0 object-cover" 
+      />
+
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 text-center">
+        <div>
+          <h1 className='text-[48px] p-2 font-medium mb-3 bg-black bg-opacity-10  rounded-sm'>
+            <span className="text-[#32cd32]">E</span>
+            <span className="text-[#f35588]">n</span>
+            <span className="text-[#ffca3a]">j</span>
+            <span className="text-[#00aeff]">o</span>
+            <span className="text-[#32cd32]">y</span> Learning With Us
+          </h1>
+        </div>
+        <p className="max-w-[600px] mb-6 p-5 text-xl bg-black bg-opacity-10  rounded-sm" lang="jp">
+          ふれあい保育・感謝・思いやり
+        </p>
+        <a href="" className="inline-block 
+  bg-[#17c528] 
+  text-white 
+  px-10 py-4 
+  rounded-full 
+  border-none 
+  cursor-pointer 
+  transition-all 
+  duration-300 
+  hover:translate-y-[-3px] 
+  hover:shadow-xl 
+  active:translate-y-[-1px] 
+  active:shadow-lgpx-[25px] py-[10px] bg-[#17c528] text-white rounded-[50px] text-4">
+          <i className=""></i>
+          こども学園での保育での保育活動
+        </a>
+      </div>
+    </div>
+  );
 };
